d23f994888b7d6196c6d2928e56146154a3c8efe
Tenshihiko Arai <arai.tenshi@gmail.com>
2025-01-21 23:00:57 +0100

    add home and site-header components with routing and styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SiteHeaderComponent } from "./site-header/site-header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SiteHeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'tptrackers';
---
+++ src/app/site-header/site-header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tpt-site-header',
+  templateUrl: './site-header.component.html',
+  styleUrl: './site-header.component.css'
+})
+export class SiteHeaderComponent {
+
+}
