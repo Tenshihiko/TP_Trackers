8285ebbfa04d59610248a5052ae53d34f1e332be
Tenshihiko Arai <arai.tenshi@gmail.com>
2025-01-22 23:49:47 +0100

    add: integrate bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+/// <reference types="@angular/localize" />
+
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/components/app.config';
 import { AppComponent } from './app/components/app.component';
