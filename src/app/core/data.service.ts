1d1ffd5ab296c171884ebe69a472bd5ffc8f638c
Angelina Sadokhina <angelina.sadokhina@spglobal.com>
2025-01-24 00:46:16 +0100

    update: refactor DataService methods to return Observables and improve data handling

--- src/app/core/data.service.ts
+++ src/app/core/data.service.ts
@@ -1,42 +1,46 @@
+import { Injectable } from "@angular/core";
 import { Books } from "./data/books.data";
-import { clothesTags } from "./data/clothestags.data";
 import { Companions } from "./data/companions.data";
 import { Relics } from "./data/relics.data";
-import { SideStory, Story, TravelersNote } from "./models";
+import { Companion, Relic, SideStory, Story, TravelersNote } from "./models";
+import { Observable, of } from "rxjs";
 
+@Injectable({
+    providedIn: 'root',
+  })
 export class DataService{
 
-    getStories(scene: sceneTagEnum | null): Story[] {
+    getStories(scene: sceneTagEnum | null): Observable<Story[]> {
         if(!scene)
-            return Books.Stories;
+            return of(Books.Stories);
 
-        return Books.Stories.filter(story => story.scenetag === scene);
+        return of(Books.Stories.filter(story => story.scenetag === scene));
     }
 
-    getSideStoriesByStory(storyId: string): SideStory[]{
-        return Books.SideStories.filter(sidestory => sidestory.parentbook.id === storyId);
+    getSideStoriesByStory(storyId: string): Observable<SideStory[]>{
+        return of(Books.SideStories.filter(sidestory => sidestory.parentbook.id === storyId));
     }
 
-    getTravelersNotes(): TravelersNote[]{
-        return Books.TravellersNotes;
+    getTravelersNotes(): Observable<TravelersNote[]>{
+        return of(Books.TravellersNotes);
     }
 
-    getRelics(stars: number | null, scene: sceneTagEnum | null, clothetags: clothesTagEnum[] | null){
-        return Relics.All.filter(relic =>
+    getRelics(stars: number | null, scene: sceneTagEnum | null, clothetags: clothesTagEnum[] | null): Observable<Relic[]>{
+        return of(Relics.All.filter(relic =>
             (!stars || relic.stars === stars)
             && (!scene || relic.scenetag === scene)
             && (!clothetags || clothetags.length == 0
                 || ((clothetags.length < 1 || relic.clothesTags.find(ct => ct === clothetags[0]))
                 && (clothetags.length < 2 || relic.clothesTags.find(ct => ct === clothetags[1]))))
-        );
+        ));
     }
 
-    getCompanions(bookid: string | null, gifttags: giftTagEnum[] | null){
-        return Companions.All.filter(companion => 
+    getCompanions(bookid: string | null, gifttags: giftTagEnum[] | null): Observable<Companion[]>{
+        return of(Companions.All.filter(companion => 
         (!bookid || companion.book?.id === bookid)
         && (!gifttags || gifttags.length == 0
             || ((gifttags.length < 1 || companion.gifttags.find(gt => gt === gifttags[0]))
             && (gifttags.length < 2 || companion.gifttags.find(gt => gt === gifttags[1]))))
-        );
+        ));
     }
 }
