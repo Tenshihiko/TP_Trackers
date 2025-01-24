42cf117ec7eee3f3b093c3252fa9e760ef54c963
Angelina Sadokhina <angelina.sadokhina@spglobal.com>
2025-01-24 01:51:25 +0100

    update: enhance Books component with filtering functionality and responsive layout

--- src/app/components/books/books.component.ts
+++ src/app/components/books/books.component.ts
@@ -1,11 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { SceneTag, Story, TravelersNote } from '../../core/models';
+import { DataService } from '../../core/data.service';
+import { SceneTags } from '../../core/data/scenetags.data'
+import { sceneTagEnum } from '../../core/enums';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'tpt-books',
-  imports: [],
   templateUrl: './books.component.html',
-  styleUrl: './books.component.scss'
+  styleUrls: ['./books.component.scss'],
+  imports: [
+    CommonModule,
+    FormsModule,
+    
+  ],
 })
-export class BooksComponent {
+export class BooksComponent implements OnInit {
+  stories: Story[] = [];
+  travelersNotes: TravelersNote[] = [];
+  sceneFilter: sceneTagEnum | null = null;
+  sceneTags: SceneTag[] = [];
 
+  constructor(private dataService: DataService) {
+    this.sceneTags = SceneTags.All;
+  }
+
+  ngOnInit(): void {
+    this.fetchData();
+  }
+
+  fetchData(): void {
+    this.dataService.getStories(this.sceneFilter).subscribe(stories => {
+      this.stories = stories;
+    });
+
+    this.dataService.getTravelersNotes(this.sceneFilter).subscribe(notes => {
+      this.travelersNotes = notes;
+    });
+  }
+
+  onFilterChange(newSceneFilter: sceneTagEnum | null): void {
+    this.sceneFilter = newSceneFilter;
+    this.fetchData();
+  }
 }
